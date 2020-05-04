b15af7e4aed3ef471652a89a6ccf0d834012f8ce
waldekglaz <waldekglaz@gmail.com>
2020-05-04 18:38:55 +0100

    topbar, navbar, hero, support section html

---
+++ javascript/hamburger.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('nav');
+const burger = document.querySelector('.hamburger');
+
+burger.addEventListener('click', function () {
+    nav.classList.toggle('active')
+    burger.classList.toggle('active')
+
+})
